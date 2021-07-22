9d6f6fd0f462c546282b259d257644e34bfb47b8
Anton <urt.sidorenko@yandex.ru>
2021-07-22 15:58:47 +0300

    refactoring Header page with TypeScript

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,15 @@
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import s from './Header.module.css'
 
-const Header = (props) => {
 
+interface HeaderPropsType {
+	isAuth: boolean
+	login: string | null
+	logoutUser: () => void
+}
+
+const Header = (props: HeaderPropsType) => {
 	return (
 		<header className={s.header}>
 			<div className={s.header__item}>
---
+++ src/components/Header/HeaderContainer.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Header from './Header'
+import * as axios from 'axios'
+import { connect, ConnectedProps } from 'react-redux'
+import { AppStateType } from '../../redux/redux-store'
+import { setAuthUser, getAndSetAuthUser, logoutUser } from '../../redux/auth-reducer'
+
+
+interface HeaderMapStateToProps {
+	login: string | null
+	isAuth: boolean
+}
+
+
+
+class HeaderContainer extends React.Component<HeaderPropsFromReduxType> {
+
+	render() {
+		return (
+			<Header {...this.props} />
+		)
+	}
+}
+
+const mapStateToProps = (state: AppStateType): HeaderMapStateToProps => ({
+	login: state.auth.login,
+	isAuth: state.auth.isAuth,
+})
+
+let connector = connect(mapStateToProps, { getAndSetAuthUser, logoutUser })
+
+type HeaderPropsFromReduxType = ConnectedProps<typeof connector>
+
+export default connector(HeaderContainer);
